8281eb8e930ebe0e0431352f6de0efa8ecc9eca5
Muatasim Ornob Kamal <31787801+Muatasim@users.noreply.github.com>
2019-02-01 23:07:14 +1000

    if conditions for promotion images

--- AddPromotion.aspx.cs
+++ AddPromotion.aspx.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
+using System.IO;
+using System.Configuration;
+using System.Web.Services;
 
 namespace Grocery_Demo
 {
@@ -17,8 +20,7 @@ namespace Grocery_Demo
                 string username = Session["UsernameAdmin"].ToString();
                 string password = Session["PasswordAdmin"].ToString();
                 string branch = Session["BranchAdmin"].ToString();
-                string CS;
-                CS = "Data Source=DESKTOP-UEA1TMT;Initial Catalog=Grocery_Demo;Integrated Security=True";
+                string CS = ConfigurationManager.ConnectionStrings["Grocery_DemoConnectionString"].ConnectionString;
                 SqlConnection con = new SqlConnection(CS);
                 SqlCommand cmd = new SqlCommand("AdminValidation", con);
                 cmd.CommandType = System.Data.CommandType.StoredProcedure;
@@ -41,8 +43,79 @@ namespace Grocery_Demo
                 Response.Redirect("SignIn.aspx");
             }
 
-            //Label1.Text = Session["BranchAdmin"].ToString();
-            //Label2.Text = Session["UsernameAdmin"].ToString();
+            Label1.Text = Session["BranchAdmin"].ToString();
+            Label2.Text = Session["UsernameAdmin"].ToString();
+
+            ShowUploadedImages();
+        }
+
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+            HttpPostedFile postedfile = FileUpload1.PostedFile;
+            string filename = Path.GetFileName(postedfile.FileName);
+            string fileextension = Path.GetExtension(filename);
+            int filesize = postedfile.ContentLength;
+
+            if (FileUpload1.HasFile)
+            {
+                if (fileextension.ToLower() == ".jpg" || fileextension.ToLower() == ".jpeg" || fileextension.ToLower() == ".bmp" ||
+                fileextension.ToLower() == ".gif" || fileextension.ToLower() == ".png")
+                {
+                    FileUpload1.SaveAs(Server.MapPath("Promotion_Image//" + FileUpload1.FileName));
+
+                    Label3.Text = "Image Uploaded Successfully";
+                }
+
+                else if (fileextension.ToLower() != ".jpg" || fileextension.ToLower() != ".jpeg" || fileextension.ToLower() != ".bmp" ||
+                fileextension.ToLower() != ".gif" || fileextension.ToLower() != ".png")
+                {
+
+                    Label3.Text = "Please Upload Image Only";
+                }
+                
+            }
+
+            if (!FileUpload1.HasFile)
+            {
+                Label3.Text = "Please Select your file";
+            }
+            
+            ShowUploadedImages();
+
+        }
+
+
+         private void  ShowUploadedImages()
+        {
+            string filters = "*.jpg;*.png;*.gif;*.jpeg";
+            string Path = ConfigurationManager.AppSettings["FilePath"].ToString();
+
+            List<String> images = new List<string>();
+
+            foreach (string filter in filters.Split(';'))
+            {
+                FileInfo[] fit = new DirectoryInfo(this.Server.MapPath(Path)).GetFiles(filter);
+                foreach (FileInfo fi in fit)
+                {
+                    images.Add(String.Format(Path + "/{0}", fi));
+                }
+            }
+
+            RepeaterImages.DataSource = images;
+            RepeaterImages.DataBind();
+
+        }
+                
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            var imagePath = ((System.Web.UI.WebControls.WebControl)sender).CssClass;
+            var filePath = Server.MapPath(imagePath);
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+
+            ShowUploadedImages();
         }
     }
 }
